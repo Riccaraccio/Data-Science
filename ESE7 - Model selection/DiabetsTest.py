5b49e41361ad087f06bd2d6d25597c0dff4752f2
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-03-07 10:09:56 +0100

    Added practiacal 7

---
+++ ESE7 - Model selection/DiabetsTest.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+import scipy.optimize
+
+def count_non_zero(array):
+    count = 0
+    for num in array:
+        if num != 0:
+            count += 1
+    return count
+
+def calculate_aic(beta, sigma_sq, y_true, x):
+    n = len(y_true)
+    residuals = y_true - np.dot(x, beta)
+    n_params = count_non_zero(beta)
+    log_likelihood = -0.5 * n * np.log(2 * np.pi * sigma_sq) - 0.5 * np.sum(residuals ** 2) / sigma_sq
+
+    aic =  2*n_params - 2*log_likelihood
+    return aic
+
+def calculate_bic(beta, sigma_sq, y_true, x):
+    n = len(y_true)
+    residuals = y_true - np.dot(x, beta)
+    log_likelihood = -0.5 * n * np.log(2 * np.pi * sigma_sq) - 0.5 * np.sum(residuals ** 2) / sigma_sq
+    n_params = count_non_zero(beta)
+    bic =  np.log(n)*n_params - 2*log_likelihood
+    return bic
+
+# Load the diabetes dataset
+diabetes = load_diabetes()
+X = diabetes.data
+y = diabetes.target
+
+# Pad X with ones for the intercept term
+X = np.pad(X, ((0, 0), (0, 1)), mode='constant', constant_values=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+def model_construction(X, y) -> np.ndarray:
+    
+    # YOUR CODE HERE ########################################
+    
+    
+    return np.zeros(X.shape[1])
+    
+    #########################################################
+
+# Train the model
+beta = model_construction(X_train, y_train)
+
+if beta.shape != (X_train.shape[1],):
+    raise ValueError("The shape of x_parameters is incorrect.")
+
+print("Your model parameters are: ",beta)
+
+print("Number of non-zero parameter:", count_non_zero(beta))
+
+sigma_sq = mean_squared_error(y_train, X_train @ beta)
+
+model_aic= calculate_aic(beta, sigma_sq, y_test, X_test)
+model_bic = calculate_bic(beta, sigma_sq, y_test, X_test)
+
+print("The AIC of your model is: {:.4f}".format(model_aic))
+print("The BIC of your model is: {:.4f}".format(model_bic))
+
+model_overall_score = model_aic + model_bic
+print("The overall score of your model is: {:.6f}. Do you think you can do better?".format(model_overall_score))
+
+# plt.plot(range(X_test.shape[0]), y_test, label="True values")
+# plt.plot(range(X_test.shape[0]), X_test@ beta, label="True values")
+
+plt.bar(range(1,beta.shape[0]+1), beta, label="Coefficients")
+# plt.scatter(X_test @ beta, y_test)
+# plt.plot([0, 350], [0, 350], 'r')
+# plt.xlabel("Predicted values")
+# plt.ylabel("True values")
+# plt.axis('square')
+# plt.xlim(0, 350)
+# plt.ylim(0, 350)
+plt.show()  
---
+++ ESE7 - Model selection/EvaluateScores.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+import pandas as pd
+
+def count_non_zero(array):
+    count = 0
+    for num in array:
+        if num != 0:
+            count += 1
+    return count
+
+def calculate_aic(beta, sigma_sq, y_true, x):
+    n = len(y_true)
+    residuals = y_true - np.dot(x, beta)
+    n_params = count_non_zero(beta)
+    log_likelihood = -0.5 * n * np.log(2 * np.pi * sigma_sq) - 0.5 * np.sum(residuals ** 2) / sigma_sq
+
+    aic =  2*n_params - 2*log_likelihood
+    return aic
+
+def calculate_bic(beta, sigma_sq, y_true, x):
+    n = len(y_true)
+    residuals = y_true - np.dot(x, beta)
+    log_likelihood = -0.5 * n * np.log(2 * np.pi * sigma_sq) - 0.5 * np.sum(residuals ** 2) / sigma_sq
+    n_params = count_non_zero(beta)
+    bic =  np.log(n)*n_params - 2*log_likelihood
+    return bic
+
+diabetes = load_diabetes()
+X_train, X_test, y_train, y_test = train_test_split(diabetes.data, diabetes.target, test_size=0.2, random_state=0)
+
+df = pd.read_excel('DATA SCIENCE _linear regression cup.xlsx')   
+df = df.rename(columns={"Beta parameters": 'beta', 'Declare your overall score': 'score'})
+
+for index, student in df.iterrows():
+    
+    # Convert the elements to float and create a NumPy array
+    student["beta"] = student["beta"] .replace('[', '').replace(']', '')
+    beta_elements = student["beta"].split()
+    beta = np.array([float(element) for element in beta_elements])
+    student["beta"] = beta
+    # Compute the number of non-zero parameters
+    student["n_params"] = count_non_zero(student["beta"])
+    
+    # predict y
+    y_pred = np.dot(X_test, student["beta"])
+    
+    # Compute the mean squared error
+    student["mse"] = mean_squared_error(y_test, y_pred)
+    
+    # Compute the AIC and BIC
+    student["aic"] = calculate_aic(student["beta"], student["mse"], y_test, X_test)
+    student["bic"] = calculate_bic(student["beta"], student["mse"], y_test, X_test)
+    student["real score"] = student["aic"] + student["bic"] 
+
+    # Update the DataFrame
+    df.loc[index, "real score"] = student["real score"]
+
+# Sort the DataFrame by the real score
+df = df.sort_values(by="real score")
+
+# Select the top n students
+n = 2  
+top_students = df.head(n)
+
+# Create a new DataFrame with selected columns
+results_df = top_students[["Nome", "real score", "beta"]]
+
+# plot the top n students scores
+plt.bar(range(1, n+1), results_df["real score"])
+plt.title("Score of the top {} students".format(n))
+plt.ylabel("Score")
+plt.xticks(range(1, n+1))
+
+for i in range(1, n+1):
+    plt.text(i, results_df.loc[i-1,"real score"] + 0.5, results_df.loc[i-1,"Nome"], ha='center')
+plt.ylim(min(results_df["real score"]) - 1, max(results_df["real score"]) + 1)
+plt.show()
+
+
+# COMPARE BETA PARAMETERS
+fig, ax = plt.subplots(1, n)
+# Initialize variables to store min and max values of beta across all students
+min_beta = np.inf
+max_beta = -np.inf
+
+for index, student in results_df.iterrows():
+    # Convert the elements to float and create a NumPy array
+    student["beta"] = student["beta"].replace('[', '').replace(']', '')
+    beta_elements = student["beta"].split()
+    beta = np.array([float(element) for element in beta_elements])
+    student["beta"] = beta
+    
+    # Update min and max beta values
+    min_beta = min(min_beta, np.min(beta))
+    max_beta = max(max_beta, np.max(beta))
+    
+    ax[index].bar(range(len(student["beta"])), student["beta"])
+    ax[index].set_title(student["Nome"])
+    
+for ax in ax:
+    ax.set_ylim(min_beta, max_beta)
+    
+plt.show()
+
+
+# COMPARE MODEL PERFORMANCE
+fig, ax = plt.subplots(1, n)
+
+for index, student in results_df.iterrows():
+    # Convert the elements to float and create a NumPy array
+    student["beta"] = student["beta"].replace('[', '').replace(']', '')
+    beta_elements = student["beta"].split()
+    beta = np.array([float(element) for element in beta_elements])
+    student["beta"] = beta
+    
+    # predict y
+    y_pred = np.dot(X_test, student["beta"])
+    
+    ax[index].scatter(y_pred, y_test)
+    ax[index].plot([0, 350], [0, 350], 'r')
+    ax[index].set_title(student["Nome"])
+    ax[index].set_xlabel("Predicted values")
+    ax[index].set_ylabel("True values")
+    ax[index].axis('equal')
+    
+plt.show()
