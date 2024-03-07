b7cf81fe9cacf0eda9a934cd3a64246fc04aa98a
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-03-07 09:51:08 +0100

    Delete CorrelationMatices.py and update HousingCorrelation.py

--- ESE2 - SVD/HousingCorrelation.py
+++ ESE2 - SVD/HousingCorrelation.py
@@ -14,10 +14,15 @@ plt.xlabel('Median Income (x1e4)') #set the x-axis label
 plt.ylabel('Median House Value (x1e5)') #set the y-axis label
 plt.show()
 
-# housing_data = housing_data - np.mean(housing_data, axis=0) #center the data
+#pad housing_data with ones for the intercept term
+housing_data = np.pad(housing_data, ((0, 0), (0, 1)), mode='constant', constant_values=1)
 
 U, S, Vt = np.linalg.svd(housing_data, full_matrices=False) # perform the SVD
 x = Vt.T @ np.linalg.inv(np.diag(S)) @ U.T @ housing_value # fit parameters influence on the data
+
+plt.plot(housing_value, c="k")
+plt.plot(housing_data @ x, c="r")
+plt.show()
 
 x_tick = range(len(x)-1)+np.ones(len(x)-1) #create the x-ticks
 plt.bar(x_tick, x[:-1], width=0.5) #plot the bar chart
