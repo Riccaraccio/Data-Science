780be6a2d9f7ae499709e656601706de7eb077f9
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-03-11 11:36:37 +0100

    Refactor PCA initialization and plot first two principal components

--- ESE3 - PCA/DigitsPCA.py
+++ ESE3 - PCA/DigitsPCA.py
@@ -18,48 +18,27 @@ plt.show()
 # We import the PCA object from the sklearn package
 from sklearn.decomposition import PCA
 
-pca = PCA(n_components=64) # we select all the features for now
+# initialize pca object
+pca = PCA() # we select all the features for now
 pca.fit(digits_images)
 
-# We use the attributes of PCA to find the explained variance in percentage
-variance_ratio = pca.explained_variance_ratio_ * 100
+# We calculate the matrix U, which contains the directions of the PCs
+principal_components = pca.components_.T
 
-fig, axs = plt.subplots(1,2, figsize=(8,4))
-axs[0].scatter(np.linspace(1, 64, 64), variance_ratio)
-axs[0].set_title('explained variance')
-axs[1].plot(np.cumsum(variance_ratio))
-axs[1].set_title('cumulative explained variance')
-plt.show()
-
-# We calculate the matrix A, which contains the directions of the PCs
-A = pca.components_.T
-
-# We can plot the first PC
-PCindex = 0
-
-plt.imshow(A[:,PCindex].reshape(8,8), cmap="grey")
-plt.title("PC number " + str(PCindex+1))
+# We can plot the first two PCs side by side
+fig, axs = plt.subplots(1,2)
+axs[0].imshow(principal_components[:,0].reshape(8,8), cmap='Greys')
+axs[0].set_title('PC number 1')
+axs[1].imshow(principal_components[:,1].reshape(8,8), cmap='Greys')
+axs[1].set_title('PC number 2')
 plt.show()
 
 # We calculate the PCA scores as Z = X A
-Z = digits_images @ A
+pca_scores = digits_images @ principal_components
 
-q = 20 # number of principal components to use
-index = 0 # the index of the digit that we want to reconstruct 
-
-digit_orig = digits_images[index,:] # original digit
-digit_rec = Z[index,:q] @ A[:,:q].T        # reconstructed digit
-
-# We can plot them side by side 
-fig, axs = plt.subplots(1,2)
-axs[0].imshow(digit_orig.reshape(8,8), cmap='Greys')
-axs[0].set_title('original')
-axs[1].imshow(digit_rec.reshape(8,8), cmap='Greys')
-axs[1].set_title('reconstructed with ' + str(q) + ' PCs')
-plt.show()
-
-plt.scatter(Z[:,0], Z[:,1], c=digits_labels, alpha=0.5, cmap="jet") 
+plt.scatter(pca_scores[:,0], pca_scores[:,1], c=digits_labels, alpha=0.5, s=10, cmap=plt.cm.get_cmap('jet', 10)) 
 # alpha = transparency
 # c = to differentiate between digits
+# cmap=plt.cm.get_cmap('jet', 10) sets the color map to jet and the number of colors to 10
 plt.colorbar()
 plt.show()
