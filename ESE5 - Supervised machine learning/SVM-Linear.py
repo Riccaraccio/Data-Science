0651ca9d6d0b2d41d4d5ec409f053925aaeb37de
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-04-03 10:32:24 +0200

    Refactor SVM-Linear.py: Clean up code and add prediction visualization

--- ESE5 - Supervised machine learning/SVM-Linear.py
+++ ESE5 - Supervised machine learning/SVM-Linear.py
@@ -7,9 +7,10 @@ X, Y = make_blobs(n_samples=500, centers=2,
                   random_state=0, cluster_std=0.60)
 
 from sklearn.svm import SVC # "Support vector classifier"
-model = SVC(kernel='linear') #A low C makes the decision surface smooth, while a high C aims at classifying all training examples correctly
+model = SVC(kernel='linear')
 model.fit(X, Y)
 
+# In order to plot the decision boundary, we need to create a grid of points
 # Define the range for the grid
 x1_range = np.linspace(np.min(X[:, 0]), np.max(X[:, 0]), 100)
 x2_range = np.linspace(np.min(X[:, 1]), np.max(X[:, 1]), 100)
@@ -19,12 +20,17 @@ xx1, xx2 = np.meshgrid(x1_range, x2_range)
 
 # Use the model to calculate the decision function over the grid
 Z_values = model.decision_function(np.c_[xx1.ravel(), xx2.ravel()])
+# ravel() converts a 2D array into a 1D array
+# np.c_ concatenates the 1D arrays into a 2D array of shape (n, 2)
 
 # Reshape the result to match the shape of xx and yy
 Z_values = Z_values.reshape(xx1.shape)
 
+# Use the model to predict the labels of the data points
+Y_pred = model.predict(X)
+
 # Create a scatter plot of the data points
-plt.scatter(X[:, 0], X[:, 1], c=Y)
+plt.scatter(X[:, 0], X[:, 1], c=Y_pred)
 
 # Add the decision boundary to the plot
 plt.contour(xx1, xx2, Z_values, levels=[0], linewidths=2, colors='k')
