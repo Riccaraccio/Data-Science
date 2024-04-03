e1af6f05b4794f835b2194263ba5b03304037819
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-04-03 10:43:49 +0200

    Refactor SVM kernel in SVM-NonLinear.py

--- ESE5 - Supervised machine learning/SVM-NonLinear.py
+++ ESE5 - Supervised machine learning/SVM-NonLinear.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn import svm
+from sklearn.svm import SVC
 from sklearn.datasets._samples_generator import make_moons #to generate data clusters
 
 X, Y = make_moons(n_samples=500, noise=0.1, random_state=0)
 
 # fit the model
 # model = svm.SVC(kernel="linear")
-# model = svm.SVC(kernel="poly")
-model = svm.SVC(kernel="rbf")
+# model = svm.SVC(kernel="poly", degree=2)
+model = SVC(kernel="rbf")
 model.fit(X, Y)
 
+# In order to plot the decision boundary, we need to create a grid of points
 # Define the range for the grid
 x1_range = np.linspace(np.min(X[:, 0]), np.max(X[:, 0]), 100)
 x2_range = np.linspace(np.min(X[:, 1]), np.max(X[:, 1]), 100)
@@ -24,7 +25,7 @@ Z_values = model.decision_function(np.c_[xx1.ravel(), xx2.ravel()])
 Z_values = Z_values.reshape(xx1.shape)
 
 plt.contour(xx1, xx2, Z_values, levels=[0], linewidths=2, colors='k')
-plt.scatter(X[:, 0], X[:, 1], c=Y)
+plt.scatter(X[:, 0], X[:, 1], c=Y) # we use the actual labels to color the points
 
 plt.show()
 
