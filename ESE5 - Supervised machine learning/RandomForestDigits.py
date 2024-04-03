39f3142dabe5d52bd23bcae261192e73cc7a318b
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-04-03 11:07:59 +0200

    removed axis labels and renamed variavbles

--- ESE5 - Supervised machine learning/RandomForestDigits.py
+++ ESE5 - Supervised machine learning/RandomForestDigits.py
@@ -11,23 +11,24 @@ fig = plt.figure()  # Create a new figure
 
 # Plot the digits: each image is 8x8 pixels
 for i in range(64):
-    ax = fig.add_subplot(8, 8, i + 1, xticks=[], yticks=[])  # Add a subplot to the figure
+    ax = fig.add_subplot(8, 8, i + 1)  # Add a subplot to the figure
+    ax.axis("off")  # Turn off the axis
     ax.imshow(digits[i].reshape(8,8), cmap="Greys")  # Display the image
 plt.show()  # Show the figure
 
 # Split the dataset into training and testing sets
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(digits, labels, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(digits, labels, random_state=0)
 
 # Create a random forest classifier with 1000 trees
 model = RandomForestClassifier(n_estimators=1000)
-model.fit(Xtrain, Ytrain)  # Train the model
-ypred = model.predict(Xtest)  # Make predictions on the test set
+model.fit(X_train, Y_train)  # Train the model
+ypred = model.predict(X_test)  # Make predictions on the test set
 
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
 # Compute the confusion matrix
-mat = confusion_matrix(Ytest, ypred)
+mat = confusion_matrix(Y_test, ypred)
 
 # Plot the confusion matrix as a heatmap
 sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False)
