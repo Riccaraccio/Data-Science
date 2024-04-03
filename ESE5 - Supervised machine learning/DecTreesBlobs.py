f5ed5e2c7777cd02bb6c7bd95a3ef640a16692fa
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-04-03 10:52:43 +0200

    Update decision tree model and test set in DecTreesBlobs.py

--- ESE5 - Supervised machine learning/DecTreesBlobs.py
+++ ESE5 - Supervised machine learning/DecTreesBlobs.py
@@ -1,38 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
-import seaborn as sns
 
 # Generate synthetic data using make_blobs
-X, Y = make_blobs(n_samples=400, centers=4, random_state=0, cluster_std=1.0)
+X, Y = make_blobs(n_samples=400, centers=3, random_state=0, cluster_std=1.0)
 
 # Plot the data points
 plt.scatter(X[:, 0], X[:, 1], c=Y)
 
 # Split the data into a training set and a test set
-X_fit = X[:300, :]
-Y_fit = Y[:300]
+X_train = X[:300,:]
+Y_train = Y[:300]
 
-X_cv = X[300:, :]
-Y_cv = Y[300:]
+X_test = X[300:,:]
+Y_test = Y[300:]
 
 from sklearn.tree import DecisionTreeClassifier
 
 # Create a decision tree classifier
-tree = DecisionTreeClassifier()
+model = DecisionTreeClassifier()
 
 # Train the decision tree classifier on the training set
-tree.fit(X_fit, Y_fit)
+model.fit(X_train, Y_train)
 
 # Define the range for the grid
-x1_range = np.linspace(np.min(X_fit[:, 0]), np.max(X_fit[:, 0]), 100)
-x2_range = np.linspace(np.min(X_fit[:, 1]), np.max(X_fit[:, 1]), 100)
+x1_range = np.linspace(np.min(X_train[:, 0]), np.max(X_train[:, 0]), 100)
+x2_range = np.linspace(np.min(X_train[:, 1]), np.max(X_train[:, 1]), 100)
 
 # Create a meshgrid
 xx1, xx2 = np.meshgrid(x1_range, x2_range)
 
 # Use the trained model to calculate the decision function over the grid
-Z_values = tree.predict(np.c_[xx1.ravel(), xx2.ravel()])
+Z_values = model.predict(np.c_[xx1.ravel(), xx2.ravel()])
 Z_values = Z_values.reshape(xx1.shape)
 
 # Plot the decision boundaries
@@ -40,12 +39,13 @@ plt.contourf(xx1, xx2, Z_values, alpha=0.3)
 plt.show()
 
 # Use the trained model to predict the labels of the test set
-Y_cv_pred = tree.predict(X_cv)
+Y_pred = model.predict(X_test)
 
 from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 # Calculate the confusion matrix
-mat = confusion_matrix(Y_cv, Y_cv_pred)
+mat = confusion_matrix(Y_test, Y_pred)
 
 # Plot the confusion matrix
 sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False) #annot=True to annotate cells, fmt='d' to disable scientific notation
