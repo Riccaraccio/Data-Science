6410d568271c795163d4a5e677474a7ac2f249a1
Riccaraccio <riccardo.caraccio@mail.polimi.it>
2024-04-10 11:54:28 +0200

    Added comments

--- ESE7 - Model selection/CrossValidation.py
+++ ESE7 - Model selection/CrossValidation.py
@@ -7,18 +7,21 @@ from sklearn.model_selection import train_test_split
 # Load the California housing dataset
 housing_data, housing_value = fetch_california_housing(return_X_y=True)
 
-housing_data = housing_data[:150]
+housing_data = housing_data[:150] # only use the first 150 samples
 housing_value = housing_value[:150]
-# fig, ax = plt.subplots(1,3)
-# for i in range(3):
-#     # Split the data into training and test sets
-#     X_train, X_test, y_train, y_test = train_test_split(housing_data, housing_value, test_size=0.3, random_state=i)
 
-#     # perform pinv regression
-#     beta_pinv = np.linalg.pinv(X_train) @ y_train
+# Visualize the fact that different random states give different results
+fig, ax = plt.subplots(1,3)
+for i in range(3):
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(housing_data, housing_value, test_size=0.3, random_state=i)
 
-#     ax[i].bar(range(len(beta_pinv)), beta_pinv)
-# plt.show()
+    # perform pinv regression
+    beta_pinv = np.linalg.pinv(X_train) @ y_train
+
+    ax[i].bar(range(len(beta_pinv)), beta_pinv)
+    ax[i].set_ylim(-4, 4)
+plt.show()
 
 # perform k fold cross validation
 values = (1, 10, 100)
@@ -44,6 +47,7 @@ for i in range(len(values)):
     
     ax[0,i].bar(range(len(beta_pinv)), beta_pinv)
     ax[0,i].set_title('PINV {}'.format(values[i]))
+    ax[0,i].set_ylim(-4, 4)
     ax[1,i].bar(range(len(beta_lasso)), beta_lasso)
     ax[1,i].set_title('Lasso {}'.format(values[i]))
     
