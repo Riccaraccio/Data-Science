187ed41d6ca24e7db83c1dc4197c94ba3c289968
Riccardo Caraccio <riccardo.caraccio@mail.polimi.it>
2025-03-19 12:37:31 +0100

    Simplified variable names

--- ESE-04-Unsupervised machine learning/ImageColorCompression.py
+++ ESE-04-Unsupervised machine learning/ImageColorCompression.py
@@ -17,49 +17,48 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from sklearn.cluster import MiniBatchKMeans
 
-# Load and reshape image
-image = imread("ESE - 04 - Unsupervised machine learning/StillLife.jpg")
-data = image.reshape(image.shape[0]*image.shape[1], image.shape[2])  # Flatten to pixel array
+# Sample random s# Load image and prepare data
+image = imread("ESE-04-Unsupervised machine learning/StillLife.jpg")
+pixels = image.reshape(-1, image.shape[2])  # Flatten to pixel array
 
-def plot_pixels(data, title, colors=None, N=10000):
-   if colors is None:
-       colors = data
-   
-   # Sample random subset of pixels
-   rng = np.random.RandomState(0)
-   i = rng.permutation(data.shape[0])[:N]
-   colors = colors[i]/255  # Normalize colors
-   
-   R, G, B = data[i].T  # Extract RGB components
-   
-   # Create scatterplots
-   fig, ax = plt.subplots(1, 2)
-   ax[0].scatter(R, G, c=colors, marker='.')
-   ax[0].set(xlabel='Red', ylabel='Green', xlim=(0, 255), ylim=(0, 255))
+def plot_colors(data, title, colors=None, samples=10000):
+    # Sample random pixels for visualization
+    if colors is None:
+        colors = data
+    
+    rng = np.random.RandomState(0)
+    indices = rng.permutation(len(data))[:samples]
+    
+    R, G, B = data[indices].T  # Extract RGB components
+    norm_colors = colors[indices]/255  # Normalize colors
+    
+    # Create side-by-side scatterplots
+    fig, ax = plt.subplots(1, 2)
+    ax[0].scatter(R, G, c=norm_colors, marker='.')
+    ax[0].set(xlabel='Red', ylabel='Green', xlim=(0, 255), ylim=(0, 255))
 
-   ax[1].scatter(R, B, c=colors, marker='.')
-   ax[1].set(xlabel='Red', ylabel='Blue', xlim=(0, 255), ylim=(0, 255))
+    ax[1].scatter(R, B, c=norm_colors, marker='.')
+    ax[1].set(xlabel='Red', ylabel='Blue', xlim=(0, 255), ylim=(0, 255))
 
-   fig.suptitle(title, size=20)
-   plt.show()
+    fig.suptitle(title, size=20)
+    plt.show()
 
-# Show original color distribution
-plot_pixels(data, title='Input color space: 16 million possible colors')
+# Show original colors
+plot_colors(pixels, title='Original: 16 million possible colors')
 
-# Perform color quantization
-kmeans = MiniBatchKMeans(16)  # Create 16-color palette
-kmeans.fit(data)
-new_colors = kmeans.cluster_centers_[kmeans.predict(data)]
-new_colors = new_colors.astype(int)
+# Perform color quantization with K-means
+kmeans = MiniBatchKMeans(n_clusters=16)
+kmeans.fit(pixels)
+reduced_colors = kmeans.cluster_centers_[kmeans.predict(pixels)].astype(int)
 
 # Show reduced color distribution
-plot_pixels(data, colors=new_colors, title="Reduced color space: 16 colors")
+plot_colors(pixels, colors=reduced_colors, title="Reduced: 16 colors")
 
 # Compare original and recolored images
-image_recolored = new_colors.reshape(image.shape)
+recolored = reduced_colors.reshape(image.shape)
 fig, ax = plt.subplots(1, 2)
 ax[0].imshow(image)
 ax[0].set_title('Original Image')
-ax[1].imshow(image_recolored)
+ax[1].imshow(recolored)
 ax[1].set_title('16-color Image')
 plt.show()
